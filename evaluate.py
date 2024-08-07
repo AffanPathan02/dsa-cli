4c23f94b86c251ca454659b6124d189d3ae6c37c
AffanPathan02 <pathan.affan02@gmail.com>
2024-08-07 07:58:04 +0530

    added evalutaion engine. need fixes

--- evaluate.py
+++ evaluate.py
@@ -1,2 +1,13 @@
-def evaluate_engine():
-    print("evaluating")
+import os
+import subprocess
+def evaluate_engine(expected_output):
+    os.system(f"gcc sample.c")
+    command="./a.out"
+    result=subprocess.check_output(command,shell=True, text=True)
+    if expected_output == result:
+        print("answer is corrected")
+    else:
+        print("solution is wrong")
+
+
+    
