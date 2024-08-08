53c32e1f92ffc68de301c32814c71717e741540f
AffanPathan02 <pathan.affan02@gmail.com>
2024-08-08 07:31:55 +0530

    removed comments

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
-import os
 import subprocess
 
 
 def setup_engine(setup_env):
     if setup_env == "c":
-        #        os.system("./scripts/install-c.sh")
         subprocess.run(['sh', './scripts/install-c.sh'])
     elif setup_env == "java":
-        #        os.system("./scripts/install-java.sh")
         subprocess.run(['sh', './scripts/install-java.sh'])
